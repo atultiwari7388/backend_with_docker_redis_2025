f88751518679983050d198ec671504ca43fa4360
Atul Tiwari <tiwariatul9526@gmail.com>
2024-12-29 21:11:45 +0530

    2. models create

---
+++ models/todo.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const todoSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+
+  description: {
+    type: String,
+    required: true,
+  },
+});
+
+const TodoModel = mongoose.model("Todo", todoSchema);
+
+export default TodoModel;
